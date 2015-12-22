38c166f311d2541e6119fb37f3aa08660c684184
dmitry <silentlagoon@gmail.com>
2015-12-22 18:16:00 +0400

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 'On');
+require_once(dirname(__FILE__) . '/Parser.php');
+
+$parser = new WLP_Parser('test.lua');
+$data = $parser->toArray();
+
+foreach($data["WRATH_VOTE"] as $key=>$value) {
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+}
+
